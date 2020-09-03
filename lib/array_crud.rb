41f62739994c4337787444b3aa90e8b3c345a9c3
Kevin Glasgow <kglas001@gmail.com>
2020-09-03 18:11:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["Boots", "Odi", "Kona"]
+  array = ["Boots", "Odi", "Kona", "Jeff"]
 end
 
 def add_element_to_end_of_array(array, element)
